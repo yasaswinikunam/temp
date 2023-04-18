8ae13d571aeb222130a24d4cd39ed9216f746bad
yasaswinikunam <130962051+yasaswinikunam@users.noreply.github.com>
2023-04-18 12:57:16 +0530

    new one

--- largest.py
+++ largest.py
@@ -1,10 +1,10 @@
-num1=10
-num2=14
-num3=12
+num1=int(input("Enter number:"))
+num2=int(input("Enter number:"))
+num3=int(input("Enter number:"))
 if(num1>=num2)and (num1>=num3):
   largest=num1
-  elif(num2>=num1)and(num2>=num3):
-    largest=num2
-    else:
-      largest=num3
-      print("The largest number is",largest)
+elif(num2>=num1)and(num2>=num3):
+  largest=num2
+else:
+  largest=num3
+print("The largest number is",largest)
