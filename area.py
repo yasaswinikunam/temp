bfc1796fb0a75850dbec58fec9556a69eba976fb
yasaswinikunam <130962051+yasaswinikunam@users.noreply.github.com>
2023-04-19 12:42:25 +0530

    Create area.py

---
+++ area.py
@@ -0,0 +1,12 @@
+l=int(input("Enter rectangle's length: "))
+b=int(input("Enter rectangle's breadth: "))
+rect_area=l*b
+print("The area of rectangle is:",rect_area)
+s=int(input("Enter side of a square:"))
+square_area=s*s
+print("The area of square is:",square_area)
+b=int(input("Enter breadth of a triangle:"))
+h=int(input("Enter height of the triangle:"))
+tri_area=0.5*b*h
+print("The area of triangle is:",tri_area)
+    
